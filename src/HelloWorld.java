e621e7ff5324ca14aeaef427206db743d8e3231d
Miguel <miguel.a.guzman0009@gmail.com>
2022-08-01 16:59:05 -0500

    syntax, types, etc ex

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,72 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.print("Hello, World!");
-        System.out.print("Hello, World!");
+        System.out.print("Hello, World! \n");
+
+        //EX 1
+        int myFavoriteNumber;
+        myFavoriteNumber = 14;
+        System.out.println(myFavoriteNumber);
+
+        //EX 2
+        String myString;
+        myString = "str";
+        System.out.println(myString);
+
+        //EX 3
+        myString = "\n";
+        System.out.println(myString);
+
+        //EX 4
+//        myString = 3.14159;
+//        System.out.println(myString);
+
+        //EX 5,6,7,8,9
+        //3.14 needs to be assigned the variable "float or double"
+        float myNumber;
+        myNumber = (float) 3.14;
+        System.out.println(myNumber);
+
+        //EX 10
+        //the first block is post incremented(5 then 6) and the second is
+        //pre incremented(6 then 6
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+        //EX 11
+        //can't use java reserved word as an identifier
+
+        //EX 12
+        //the first is implicitly casting a string to an int, the second
+        //is explicitly casting a string to an int
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+
+//        int three = (int) "three";
+
+        //EX 13
+//        int x = 4;
+//        x += 5;
+//        System.out.println(x);
+
+//        int x = 3;
+//        int y = x * 4;
+//        System.out.println(y);
+
+//        int x = 10;
+//        int y = 2;
+//        x /= y;
+//        y -= x;
+
+        //EX 14
+        //number too large, doesn't matter if its smaller
+//        int f = 10000000000000;
+//        System.out.println(f);
     }
 }
