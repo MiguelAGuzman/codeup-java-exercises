a59c6bf77a8882ffc7bed2935bc63ac449f2960e
MiguelAGuzman <miguel.a.guzman0009@gmail.com>
2022-08-10 11:20:11 -0500

    Arrays ex, finished first problem

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -1,18 +1,34 @@
 import java.util.Arrays;
 
 public class ArraysExercises {
+    //Create a static method named addPerson. It should accept an
+    // array of "Person" objects, as well as a single person object
+    // to add to the passed array. It should return an array whose
+    // length is 1 greater than the passed array, with the passed
+    // person object at the end of the array.
+    public static Person[] addPerson (Person [] newPerson, Person newName) {
+        Person [] newArray = Arrays.copyOf(newPerson, newPerson.length + 1);
+        newArray[newPerson.length] = newName;
+        return newArray;
+    }
+
     public static void main(String[] args) {
-        String[] Person = {"m", "g", "7"};
-        System.out.println(Arrays.toString(Person));
+        //create 3 "Person" objects and assign a new instance of the
+        // "Person" class to each element
+        Person [] persons = {new Person("Bob Smith"),
+                new Person("Jill Williams"),
+                new Person("Miguel Guzman")};
+        //Iterate through the array and print out each person's name
+        for (Person getPerson : persons) {
+            System.out.println(getPerson.getName());
+        }
 
+        Person [] newPersonJack = addPerson(persons, new Person("Jack"));
+        for (Person getJack : newPersonJack) {
+            System.out.println(getJack.getName());
+        }
 
     }
 
-    public String Person(String firstName, String lastName, String age) {
-        return Person();
-    }
 
-    private String Person() {
-        return Person();
-    }
 }
