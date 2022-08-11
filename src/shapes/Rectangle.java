8f5b13889085ff2090d19c48ad7e6ff9c266455b
MiguelAGuzman <miguel.a.guzman0009@gmail.com>
2022-08-11 09:15:34 -0500

    int and poly ex finished

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -1,15 +1,21 @@
 package shapes;
 
 public class Rectangle {
-    private int length;
-    private int width;
+    protected int length;
+    protected int width;
 
     public Rectangle(int length, int width) {
         this.length = length;
         this.width = width;
     }
 
+    public int getArea() {
+        return this.length * this.width;
+    }
 
+    public int getPerimeter() {
+        return 2 * this.length + 2 * this.width;
+    }
 
     public void setLength(int length) {
         this.length = length;
